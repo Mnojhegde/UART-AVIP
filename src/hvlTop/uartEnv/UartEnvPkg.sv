45da95b1c5c5fe17580529bb3463759a38126dbd
Seema J Rai <109678174+seemaRai902@users.noreply.github.com>
2025-01-23 10:01:31 +0530

    Update UartEnvPkg.sv

--- src/hvlTop/uartEnv/UartEnvPkg.sv
+++ src/hvlTop/uartEnv/UartEnvPkg.sv
@@ -1,16 +1,33 @@
 `ifndef UARTENVPKG_INCLUDED_
 `define UARTENVPKG_INCLUDED_
 
+//--------------------------------------------------------------------------------------------
+// Package: UartEnvPkg
+// Includes all the files related to uart env
+//--------------------------------------------------------------------------------------------
 package UartEnvPkg;
+  
+  //-------------------------------------------------------
+  // Importing uvm packages
+  //-------------------------------------------------------  
   `include "uvm_macros.svh"
   import uvm_pkg :: *;
+
+  //-------------------------------------------------------
+  // Importing the required packages
+  //-------------------------------------------------------
   import UartGlobalPkg :: *;
   import UartTxPkg :: *;
   import UartRxPkg :: *;
 
+  //-------------------------------------------------------
+  // Including the required files
+  //-------------------------------------------------------
   `include "UartEnvConfig.sv"
   `include "UartVirtualSequencer.sv"
   `include "UartScoreboard.sv"
   `include "UartEnv.sv"
+  
 endpackage : UartEnvPkg
+
 `endif
