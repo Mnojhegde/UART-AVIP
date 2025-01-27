23c70ee069f09999073912b2fc71867b77f040aa
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-01-27 13:08:46 +0530

    Create UartTxAssertionTB.sv

---
+++ src/hvlTop/tb/uartLocalAssertionTest/UartTxAssertionTB.sv
@@ -0,0 +1,13 @@
+`ifndef UARTTXASSERTIONTB_INCLUDED_
+`define UARTTXASSERTIONTB_INCLUDED_
+
+`include "uvm_macros.svh"
+import uvm_pkg :: *;
+import UartGlobalPkg :: *;
+
+module UartTxAssertionTb;
+
+
+endmodule 
+
+`endif
