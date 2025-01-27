545d3fce3c2c45f2a0dff4b6677a99a04b441c6c
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-01-27 16:18:45 +0530

    Create UartRxAssertionTB.sv

---
+++ src/hvlTop/tb/uartLocalAssertionTest/UartRxAssertionTB.sv
@@ -0,0 +1,37 @@
+
+`ifndef UARTRXASSERTIONTB_INCLUDED_
+`define UARTRXASSERTIONTB_INCLUDED_
+
+`include "uvm_macros.svh"
+import uvm_pkg :: *;
+import UartGlobalPkg :: *;
+
+module UartRxAssertionTb;
+  logic Rx;
+  bit clk = 0;
+
+  always #5 clk = ~clk;
+  initial begin 
+    #80;
+    $finish;
+  end 
+
+  UartRxAssertions uartRxAssertions(.uartClk(clk),.uartRx(Rx));
+  
+  initial begin 
+    When_startBitisdetected_nextCycleDataTransferStarts_ThenStopBitDetected_AssertionPass();
+  end 
+
+  task  When_startBitisdetected_nextCycleDataTransferStarts_ThenStopBitDetected_AssertionPass()
+    #4  Rx = 1;
+    #10 Rx = 0;
+    #10 Rx = 1;
+    #10 Rx = 0;
+    #10 Rx = 1;
+    #10 Rx = 1;
+    #10 Rx = 0;
+    #10 Rx = 1 ;
+  endtask 
+endmodule 
+
+`endif
