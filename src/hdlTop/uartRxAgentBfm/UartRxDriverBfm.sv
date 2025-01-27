b5f5f3bb464baebd2b3a080951201edaa73c04a1
Hemanth V  Prabhu <114757994+hemanth-VP@users.noreply.github.com>
2025-01-27 16:28:32 +0530

    Update UartRxDriverBfm.sv
    added 2 tasks in drivetobfm task for driving data to uart interface

--- src/hdlTop/uartRxAgentBfm/UartRxDriverBfm.sv
+++ src/hdlTop/uartRxAgentBfm/UartRxDriverBfm.sv
@@ -38,6 +38,11 @@ interface UartRxDriverBfm (input  bit   clk,
   //Used to sample the uart data
 	
   //reg[31:0] baudRate = 9600;
+
+  //Variable: oversampling_clk
+  // clk used to sample the data
+	
+  bit oversampling_clk;
   
   //Variable: baudRate
   // Counter to keep track of clock cycles
@@ -119,9 +124,57 @@ interface UartRxDriverBfm (input  bit   clk,
   //--------------------------------------------------------------------------------------------
 
   task DriveToBfm();
+	  
+	`uvm_info(name,$sformatf("data_packet=\n%p",uartRxPacketStruct),UVM_HIGH);
+    	`uvm_info(name,$sformatf("DRIVE TO BFM TASK"),UVM_HIGH);
     
+	bclk_counter(agtcfg.oversamplingmethod);   // configure in agt config
+    
+	sample_data(uartRxPacketStruct);
 
   endtask: DriveToBfm
+
+
+   //--------------------------------------------------------------------------------------------
+  // Task: bclk_counter
+  //  This task will count the number of cycles of bclk and generate oversampling_clk to sample data
+  //--------------------------------------------------------------------------------------------
+
+  task bclk_counter(input oversamplingmethod);
+    static int countbclk = 0;
+    forever begin
+	@posedge(baudClk)
+	if(countbclk == (oversamplingmethod/2)-1) begin
+      		oversampling_clk = ~oversampling_clk;
+      		countbclk=0;
+      	end
+      	else begin
+      	countbclk = countbclk+1;
+      end
+   
+    end
+  endtask
+  
+  //--------------------------------------------------------------------------------------------
+  // Task: sample_data
+  //  This task will send the data to the uart interface based on oversampling_clk
+  //--------------------------------------------------------------------------------------------
+  
+  task sample_data(inout UartRxPacketStruct uartRxPacketStruct);
+      int total_receiving = uartRxTransaction.receivingData.size();
+     //@(posedge oversampling_clk) 
+     // tx = START_BIT;  //create enum
+	  
+     for(int receiving_number=0 ; receiving_number < total_receiving; receiving_number++) begin  
+	for( int i=0 ; i< DATA_WIDTH ; i++) begin
+      		@(posedge oversampling_clk or negedge oversampling_clk) begin
+			rx = uartRxPacketStruct.receivingData[receiving_number][i];
+      	end
+    end
+      
+    //@(posedge oversampling_clk)
+    //tx = STOP_BIT;  // create enum 
+  endtask
 
   //--------------------------------------------------------------------------------------------
   // Task: DeSerialization
