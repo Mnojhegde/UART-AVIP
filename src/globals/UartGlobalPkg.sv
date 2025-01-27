889f744d3d5299569ea1b81e77c6d816929ddf9b
Mnojhegde <109811963+Mnojhegde@users.noreply.github.com>
2025-01-27 14:32:02 +0530

    Update UartGlobalPkg.sv

--- src/globals/UartGlobalPkg.sv
+++ src/globals/UartGlobalPkg.sv
@@ -18,10 +18,10 @@ package UartGlobalPkg;
 
   typedef enum bit[31:0]{ BAUD_4800 = 32'd4800,
                           BAUD_9600 = 32'd9600,
-                          BAUD_19200 = 32'd19200 }Baud_Rate;
+                         BAUD_19200 = 32'd19200 }baud_rate_e;
 
   typedef enum bit[4:0]{ X16 = 5'd16,
-                         X13 = 5'd13}Over_Sampling;
+                        X13 = 5'd13}over_sampling_e;
 
 
   typedef enum bit[1:0]{ ONE_BIT = 1,
