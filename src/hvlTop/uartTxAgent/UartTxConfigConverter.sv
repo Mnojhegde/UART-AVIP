3db5494a10f0d53235e7af2eeabec6a86363b926
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-01-28 22:32:35 +0530

    Update UartTxConfigConverter.sv

--- src/hvlTop/uartTxAgent/UartTxConfigConverter.sv
+++ src/hvlTop/uartTxAgent/UartTxConfigConverter.sv
@@ -1,1 +1,26 @@
+`ifndef UARTTXCONFIGCONVERTER_INCLUDED_
+`define UARTTXCONFIGCONVERTER_INCLUDED_
 
+class UartTxConfigConverter extends uvm_object;
+  `uvm_object_utils(UartTxConfigConverter)
+  
+
+  extern function new( string name = "UartTxConfigConverter");
+    extern static function void fromTxClass(input UartTxAgentConfig uartTxAgentConfig, output UartConfigStruct uartConfigStruct);
+    extern static function void toTxClass(input UartConfigStruct uartConfigStruct, output UartTxAgentConfig uartTxAgentConfig);
+endclass :UartTxSeqItemConverter
+    
+
+function UartTxSeqItemConverter :: new(string name = "UartTxSeqItemConverter");
+  super.new(name);
+endfunction : new
+
+function void UartTxSeqItemConverter :: fromTxClass(input uartTxAgentConfig uartTxAgentConfig, output uartConfigStruct uartConfigStruct);
+  uartConfigStruct.uartOversamplingMethod =  uartTxAgentConfig.uartOversamplingMethod;
+  uartConfigStruct.uartBaudRate =  uartTxAgentConfig.uartBaudRate;
+ endfunction : fromTxClass
+
+
+`endif
+ 
+  
