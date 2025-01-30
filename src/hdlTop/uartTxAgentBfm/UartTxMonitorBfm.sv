fb98445588814895d93bc53b096badf19f3fac98
Seema J Rai <109678174+seemaRai902@users.noreply.github.com>
2025-01-30 11:45:16 +0530

    Update UartTxMonitorBfm.sv

--- src/hdlTop/uartTxAgentBfm/UartTxMonitorBfm.sv
+++ src/hdlTop/uartTxAgentBfm/UartTxMonitorBfm.sv
@@ -144,10 +144,10 @@ interface UartTxMonitorBfm (input  bit   clk,
   //-------------------------------------------------------
 
  // task Deserializer(output UartTxPacketStruct uartTxPacketStruct, input UartConfigStruct uartTxCfg);
- //   static int total_transmission = uartTxCfg.uartDataType;
+ //   static int total_transmission = NO_OF_PACKETS;
  //    @(negedge tx);
  //    for(int transmission_number=0 ; transmission_number < total_transmission; transmission_number++)begin 
- //      for( int i=0 ; i < uartTxCfg.uartDataType ; i++) begin
+ //      for( int i=0 ; i <  uartTxCfg.uartDataType ; i++) begin
  //    	@(posedge oversamplingClk or negedge oversamplingClk) begin
  //          uartTxPacketStruct.transmissionData[transmission_number][i] = tx;
  //        end
