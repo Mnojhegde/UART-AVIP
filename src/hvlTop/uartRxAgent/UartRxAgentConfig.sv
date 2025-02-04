26c912db9f1d3501408a9297e9aee5aa881177b6
Mnojhegde <109811963+Mnojhegde@users.noreply.github.com>
2025-02-04 15:46:07 +0530

    Update UartRxAgentConfig.sv

--- src/hvlTop/uartRxAgent/UartRxAgentConfig.sv
+++ src/hvlTop/uartRxAgent/UartRxAgentConfig.sv
@@ -7,29 +7,19 @@
 class UartRxAgentConfig extends uvm_object;
   `uvm_object_utils(UartRxAgentConfig)
   
-  //Variable: is_active
-  //Used to declare whether the agent is active or passive
   uvm_active_passive_enum is_active;
 
-  //Variable: has_coverage
-  //Used to set whether we need to create coverage or not
+  //Variables
   bit hasCoverage;
+  bit hasParity;
+  OVER_SAMPLING_E uartOverSamplingMethod;
+  BAUD_RATE_E uartBaudRate;
+  DATA_TYPE_E uartDataType;/* these enum can be randomized and can apply inline constraint in test file*/
+  PARITY_TYPE_E uartParityType;
+  rand int packetsNeeded;
+  bit parityErrorInjection;
 
  
-  //Variable: has_coverage 
-  bit hasParity;
-
-  //no. of stop bit required
-  rand STOP_BIT_E stop_bit;
-
-  //oversamping method
-  rand OVER_SAMPLING_E uartOverSamplingMethod;
-
-  //no. of data bit tranferred
-  rand DATA_TYPE_E uartDataType;
-
-  //no. of parity bit required
-  rand PARITY_TYPE_E parity_type;
 
   
   //-------------------------------------------------------
