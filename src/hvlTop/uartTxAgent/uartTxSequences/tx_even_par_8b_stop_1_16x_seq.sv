b716aff41dbbe89bec6673d0fd878ed112faa627
Sourabh <147809263+SourabhR11@users.noreply.github.com>
2025-02-05 09:52:40 +0530

    Update tx_even_par_8b_stop_1_16x_seq.sv

--- src/hvlTop/uartTxAgent/uartTxSequences/tx_even_par_8b_stop_1_16x_seq.sv
+++ src/hvlTop/uartTxAgent/uartTxSequences/tx_even_par_8b_stop_1_16x_seq.sv
@@ -1,22 +1,22 @@
 `ifndef TXEVENPAR8BSTOP116XSEQ_INCLUDED_
 `define TXEVENPAR8BSTOP116XSEQ_INCLUDED_
 
-class tx_even_par_8b_stop_1_16x_seq extends UartTxBaseSequence;
-  `uvm_object_utils(tx_even_par_8b_stop_1_16x_seq)
+class TxEvenPar8b1stop16xSeq extends UartTxBaseSequence;
+  `uvm_object_utils(TxEvenPar8b1stop16xSeq)
 
-   extern function new(string name = "tx_even_par_8b_stop_1_16x_seq");
+   extern function new(string name = "TxEvenPar8b1stop16xSeq");
   extern virtual task body();
 
-endclass : tx_even_par_8b_stop_1_16x_seq
+endclass : TxEvenPar8b1stop16xSeq
 
-function  tx_even_par_8b_stop_1_16x_seq :: new(string name= "tx_even_par_8b_stop_1_16x_seq");
+function  TxEvenPar8b1stop16xSeq :: new(string name= "TxEvenPar8b1stop16xSeq");
   super.new(name);
 endfunction : new
 
-task tx_even_par_8b_stop_1_16x_seq :: body();
+task TxEvenPar8b1stop16xSeq :: body();
   super.body();
  
-  `uvm_info(get_type_name(),$sformatf("tx_even_par_8b_stop_1_16x_seq"),UVM_LOW);
+  `uvm_info(get_type_name(),$sformatf("TxEvenPar8b1stop16xSeq"),UVM_LOW);
 
   req = UartTxTransaction :: type_id :: create("req");
 
