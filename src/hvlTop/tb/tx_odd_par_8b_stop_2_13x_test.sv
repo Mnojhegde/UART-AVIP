50c4248bb472b2138c169f195ea1e1caa1d72524
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-02-05 10:06:50 +0530

    Update tx_odd_par_8b_stop_2_13x_test.sv

--- src/hvlTop/tb/tx_odd_par_8b_stop_2_13x_test.sv
+++ src/hvlTop/tb/tx_odd_par_8b_stop_2_13x_test.sv
@@ -1,7 +1,7 @@
 `ifndef TXODDPAR8BSTOP213XTEST_INCLUDED_
 `define TXODDPAR8BSTOP213XTEST_INCLUDED_
 
-class tx_odd_par_8b_stop_2_13x_test extends UartBaseTest ;
+class TxOddPar8b2Stop13xTest extends UartBaseTest ;
   `uvm_component_utils(tx_odd_par_8b_stop_2_13x_test)
   
   tx_odd_par_8b_stop_2_13x_vseq tx_odd_par_8b_stop_2_13x_vseq_h;
@@ -19,23 +19,23 @@ class tx_odd_par_8b_stop_2_13x_test extends UartBaseTest ;
   extern  function void  setupUartTxAgentConfig();
   extern  function void  setupUartRxAgentConfig();
 
-endclass : tx_odd_par_8b_stop_2_13x_test
+endclass : TxOddPar8b2Stop13xTest
 
-function tx_odd_par_8b_stop_2_13x_test::new(string name = "tx_odd_par_8b_stop_1_16x_test", uvm_component parent = null);
+function TxOddPar8b2Stop13xTest::new(string name = "tx_odd_par_8b_stop_1_16x_test", uvm_component parent = null);
   super.new(name, parent);
 endfunction : new
 
-function void tx_odd_par_8b_stop_2_13x_test :: build_phase(uvm_phase phase);
+function void TxOddPar8b2Stop13xTest :: build_phase(uvm_phase phase);
   super.build_phase(phase);
   setupUartEnvConfig();
   uartEnv = UartEnv :: type_id :: create("uartEnv" , this);
 endfunction  : build_phase
 
- function void tx_odd_par_8b_stop_2_13x_test :: setupUartEnvConfig();
+ function void TxOddPar8b2Stop13xTest :: setupUartEnvConfig();
    super.setupUartEnvConfig();
 endfunction : setupUartEnvConfig 
 
- function void tx_odd_par_8b_stop_2_13x_test :: setupUartTxAgentConfig();
+ function void TxOddPar8b2Stop13xTest :: setupUartTxAgentConfig();
   
   uartEnvConfig.uartTxAgentConfig = UartTxAgentConfig :: type_id :: create("uartTxAgentConfig");
   uartEnvConfig.uartTxAgentConfig.is_active = UVM_ACTIVE;
@@ -49,12 +49,12 @@ endfunction : setupUartEnvConfig
 
 endfunction : setupUartTxAgentConfig
 
-function void tx_odd_par_8b_stop_2_13x_test :: setupUartRxAgentConfig();
+function void TxOddPar8b2Stop13xTest :: setupUartRxAgentConfig();
 super.setupUartRxAgentConfig();
 
 endfunction : setupUartRxAgentConfig
 
-task tx_odd_par_8b_stop_2_13x_test::run_phase(uvm_phase phase);
+task TxOddPar8b2Stop13xTest::run_phase(uvm_phase phase);
   
  tx_odd_par_8b_stop_2_13x_vseq_h = tx_odd_par_8b_stop_2_13x_vseq::type_id::create("tx_odd_par_8b_stop_2_13x_vseq_h");
   `uvm_info(get_type_name(),$sformatf("tx_odd_par_8b_stop_2_13x_test"),UVM_LOW);
