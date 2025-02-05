4d0caefbb2c8f65be1523b7431548ebbdd0ca59d
Mnojhegde <109811963+Mnojhegde@users.noreply.github.com>
2025-02-05 08:54:45 +0530

    Update UartRxConfigConverter.sv

--- src/hvlTop/uartRxAgent/UartRxConfigConverter.sv
+++ src/hvlTop/uartRxAgent/UartRxConfigConverter.sv
@@ -2,8 +2,8 @@
 `define UARTRXCONFIGCONVERTER_INCLUDED_
 
 class UartRxConfigConverter extends uvm_object;
-  `uvm_object_utils(uartRxConfigConverter)
-  extern function new( string name = "uartRxConfigConverter");
+  `uvm_object_utils(UartRxConfigConverter)
+  extern function new( string name = "UartRxConfigConverter");
   extern static function void from_Class(input UartRxAgentConfig uartRxAgentConfig, output UartConfigStruct uartConfigStruct);
 endclass :UartRxConfigConverter
     
