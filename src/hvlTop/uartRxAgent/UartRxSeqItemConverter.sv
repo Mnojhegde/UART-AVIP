f508914be3366f68679556fc91eb74a2782ced5f
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2025-02-07 01:16:56 +0530

    Update UartRxSeqItemConverter.sv

--- src/hvlTop/uartRxAgent/UartRxSeqItemConverter.sv
+++ src/hvlTop/uartRxAgent/UartRxSeqItemConverter.sv
@@ -32,13 +32,9 @@ endfunction : new
 // name -UartRxTransaction, UartRxPacketStruct 
 //--------------------------------------------------------------------------------------------
 function void UartRxSeqItemConverter :: fromRxClass(input UartRxTransaction uartRxTransaction,input UartRxAgentConfig uartRxAgentConfig, output UartRxPacketStruct uartRxPacketStruct);
-  int total_transmission = uartRxTransaction.receivingData.size();
-  for(int receiving_number=0 ; receiving_number < total_transmission; receiving_number++)begin 
     for( int i=0 ; i< uartRxAgentConfig.uartDataType ; i++) begin  
-      uartRxPacketStruct.receivingData[receiving_number][i] = uartRxTransaction.receivingData[receiving_number][i];
+      uartRxPacketStruct.receivingData[i] = uartRxTransaction.receivingData[i];
     end
-    uartRxPacketStruct.parity[receiving_number] = uartRxTransaction.parity[receiving_number];
-  end 
 endfunction : fromRxClass
     
 //--------------------------------------------------------------------------------------------
@@ -47,13 +43,9 @@ endfunction : fromRxClass
 //  name - UartRxPacketStruct,UartRxTransaction 
 //--------------------------------------------------------------------------------------------
 function void UartRxSeqItemConverter :: toRxClass(input UartRxPacketStruct uartRxPacketStruct,input UartRxAgentConfig uartRxAgentConfig,inout UartRxTransaction uartRxTransaction);
-  int total_receiving = $size(uartRxPacketStruct.receivingData);
-  uartRxTransaction.receivingData = new[total_receiving];
-  for(int receiving_number=0 ; receiving_number < total_receiving; receiving_number++)begin 
     for( int i=0 ; i<uartRxAgentConfig.uartDataType ; i++) begin
-      uartRxTransaction.receivingData[receiving_number][i] = uartRxPacketStruct.receivingData[receiving_number][i];
+      uartRxTransaction.receivingData[i] = uartRxPacketStruct.receivingData[i];
     end
-    uartRxTransaction.parity[receiving_number] = uartRxPacketStruct.parity[receiving_number];
-  end
+    uartRxTransaction.parity = uartRxPacketStruct.parity;
 endfunction
 `endif
