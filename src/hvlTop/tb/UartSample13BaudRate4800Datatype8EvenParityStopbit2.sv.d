49b231585ffecebe4787202f9f5559401e2de75c
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2025-02-12 10:12:18 +0530

    Update UartSample13BaudRate4800Datatype8EvenParityStopbit2.sv.d

--- src/hvlTop/tb/UartSample13BaudRate4800Datatype8EvenParityStopbit2.sv.d
+++ src/hvlTop/tb/UartSample13BaudRate4800Datatype8EvenParityStopbit2.sv.d
@@ -48,7 +48,7 @@ endfunction  : build_phase
 // Parameters:
 // phase - stores the current phase
 //------------------------------------------------------------------------------------------
-taskUartSample13BaudRate4800Datatype8EvenParityStopbit2:: run_phase(uvm_phase phase);
+task UartSample13BaudRate4800Datatype8EvenParityStopbit2:: run_phase(uvm_phase phase);
   UartVirtualBaseSequence :: type_id ::set_type_override(UartVirtualTransmissionSequence::get_type());
   uartVirtualBaseSequence = UartVirtualBaseSequence :: type_id :: create("uartVirtualBaseSequence");
   uartVirtualBaseSequence.print();
