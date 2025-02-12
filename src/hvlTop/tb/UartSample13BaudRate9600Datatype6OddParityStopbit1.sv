6ec4e10ce9ed96e168eefc584b9e32d99bc1ee46
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2025-02-12 10:59:59 +0530

    Update UartSample13BaudRate9600Datatype6OddParityStopbit1.sv

--- src/hvlTop/tb/UartSample13BaudRate9600Datatype6OddParityStopbit1.sv
+++ src/hvlTop/tb/UartSample13BaudRate9600Datatype6OddParityStopbit1.sv
@@ -22,7 +22,7 @@ endclass :UartSample13BaudRate9600Datatype6OddParityStopbit1
 //
 // parent - parent under which this component is created
 //--------------------------------------------------------------------------------------------
-functionUartSample13BaudRate9600Datatype6OddParityStopbit1:: new(string name = "UartSample13BaudRate9600Datatype6OddParityStopbit1" , uvm_component parent = null);
+function UartSample13BaudRate9600Datatype6OddParityStopbit1:: new(string name = "UartSample13BaudRate9600Datatype6OddParityStopbit1" , uvm_component parent = null);
   super.new(name,parent);
 endfunction  : new
 //--------------------------------------------------------------------------------------------
@@ -50,7 +50,7 @@ endfunction  : build_phase
 // Parameters:
 // phase - stores the current phase
 //------------------------------------------------------------------------------------------
-taskUartSample13BaudRate9600Datatype6OddParityStopbit1:: run_phase(uvm_phase phase);
+task UartSample13BaudRate9600Datatype6OddParityStopbit1:: run_phase(uvm_phase phase);
   UartVirtualBaseSequence :: type_id ::set_type_override(UartVirtualTransmissionSequence::get_type());
   uartVirtualBaseSequence = UartVirtualBaseSequence :: type_id :: create("uartVirtualBaseSequence");
   uartVirtualBaseSequence.print();
