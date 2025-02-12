659aefa9893f37cc4c9a8e859beb67bae575199f
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2025-02-12 10:51:00 +0530

    Update UartSample13BaudRate9600Datatype5EvenParityStopbit2.sv

--- src/hvlTop/tb/UartSample13BaudRate9600Datatype5EvenParityStopbit2.sv
+++ src/hvlTop/tb/UartSample13BaudRate9600Datatype5EvenParityStopbit2.sv
@@ -1,8 +1,10 @@
+`ifndef UARTSAMPLE13BAUDRATE9600DATATYPE5EVENPARITYSTOPBIT2_INCLUDED_
+`define UARTSAMPLE13BAUDRATE9600DATATYPE5EVENPARITYSTOPBIT2_INCLUDED_   
 //--------------------------------------------------------------------------------------------
 // Class: UartSample13BaudRate9600Datatype5EvenParityStopbit2
-// A test for 16 sampling condition
+// A test for 13 sampling condition
 //--------------------------------------------------------------------------------------------
-classUartSample13BaudRate9600Datatype5EvenParityStopbit2 extends UartBaseTest;
+class UartSample13BaudRate9600Datatype5EvenParityStopbit2 extends UartBaseTest;
    `uvm_component_utils(UartSample13BaudRate9600Datatype5EvenParityStopbit2)
     UartVirtualBaseSequence uartVirtualBaseSequence;
     //-------------------------------------------------------
@@ -20,7 +22,7 @@ endclass :UartSample13BaudRate9600Datatype5EvenParityStopbit2
 //
 // parent - parent under which this component is created
 //--------------------------------------------------------------------------------------------
-functionUartSample13BaudRate9600Datatype5EvenParityStopbit2:: new(string name = "UartSample13BaudRate9600Datatype5EvenParityStopbit2" , uvm_component parent = null);
+function UartSample13BaudRate9600Datatype5EvenParityStopbit2:: new(string name = "UartSample13BaudRate9600Datatype5EvenParityStopbit2" , uvm_component parent = null);
   super.new(name,parent);
 endfunction  : new
 //--------------------------------------------------------------------------------------------
@@ -48,7 +50,7 @@ endfunction  : build_phase
 // Parameters:
 // phase - stores the current phase
 //------------------------------------------------------------------------------------------
-taskUartSample13BaudRate9600Datatype5EvenParityStopbit2:: run_phase(uvm_phase phase);
+task UartSample13BaudRate9600Datatype5EvenParityStopbit2:: run_phase(uvm_phase phase);
   UartVirtualBaseSequence :: type_id ::set_type_override(UartVirtualTransmissionSequence::get_type());
   uartVirtualBaseSequence = UartVirtualBaseSequence :: type_id :: create("uartVirtualBaseSequence");
   uartVirtualBaseSequence.print();
