271c5c033985da354488bc0eaa801f0d4263ffe7
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2025-02-12 11:35:37 +0530

    Update UartSample13BaudRate9600Datatype7OddParityStopbit1.sv

--- src/hvlTop/tb/UartSample13BaudRate9600Datatype7OddParityStopbit1.sv
+++ src/hvlTop/tb/UartSample13BaudRate9600Datatype7OddParityStopbit1.sv
@@ -1,6 +1,8 @@
+`ifndef UARTSAMPLE13BAUDRATE9600DATATYPE7ODDPARITYSTOPBIT1_INCLUDED_
+`define UARTSAMPLE13BAUDRATE9600DATATYPE7ODDPARITYSTOPBIT1_INCLUDED_   
 //--------------------------------------------------------------------------------------------
 // Class: UartSample13BaudRate9600Datatype7OddParityStopbit1
-// A test for 16 sampling condition
+// A test for 13 sampling condition
 //--------------------------------------------------------------------------------------------
 class UartSample13BaudRate9600Datatype7OddParityStopbit1extends UartBaseTest;
    `uvm_component_utils( UartSample13BaudRate9600Datatype7OddParityStopbit1)
