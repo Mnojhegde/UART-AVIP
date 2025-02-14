8fc91ac49e86ab44207bd38af8b6eed42e46f356
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2025-02-14 09:54:19 +0530

    Update UartSample13BaudRate19200Datatype5OddParityStopbit1.sv

--- src/hvlTop/tb/UartSample13BaudRate19200Datatype5OddParityStopbit1.sv
+++ src/hvlTop/tb/UartSample13BaudRate19200Datatype5OddParityStopbit1.sv
@@ -1,20 +1,20 @@
 `ifndef  UARTSAMPLE13BAUDRATE1920DATATYPE5ODDPARITYSTOPBIT1_INCLUDED_
 `define  UARTSAMPLE13BAUDRATE19200DATATYPE5ODDPARITYSTOPBIT1_INCLUDED_   
 //--------------------------------------------------------------------------------------------
-// Class:  UartSample13BaudRate19200Datatype5OddParityStopbit1
+// Class:UartSample13BaudRate19200Datatype5OddParityStopbit1
 // A test for 13 sampling condition
 //--------------------------------------------------------------------------------------------
-class   UartSample13BaudRate19200Datatype5OddParityStopbit1 extends UartBaseTest;
-   `uvm_component_utils(   UartSample13BaudRate19200Datatype5OddParityStopbit1)
+class UartSample13BaudRate19200Datatype5OddParityStopbit1 extends UartBaseTest;
+   `uvm_component_utils(UartSample13BaudRate19200Datatype5OddParityStopbit1)
     UartVirtualBaseSequence uartVirtualBaseSequence;
     //-------------------------------------------------------
     // Externally defined Tasks and Functions
     //-------------------------------------------------------
-    extern function new(string name = "   UartSample13BaudRate19200Datatype5OddParityStopbit1" , uvm_component parent = null);
+    extern function new(string name = "UartSample13BaudRate19200Datatype5OddParityStopbit1" , uvm_component parent = null);
     extern virtual function void  build_phase(uvm_phase phase);
     extern virtual task run_phase(uvm_phase phase);
  
-endclass :   UartSample13BaudRate19200Datatype5OddParityStopbit1
+endclass :UartSample13BaudRate19200Datatype5OddParityStopbit1
 //--------------------------------------------------------------------------------------------
 // Constructor:new
 //
@@ -22,7 +22,7 @@ endclass :   UartSample13BaudRate19200Datatype5OddParityStopbit1
 //
 // parent - parent under which this component is created
 //--------------------------------------------------------------------------------------------
-function UartSample13BaudRate19200Datatype5OddParityStopbit1:: new(string name = "   UartSample13BaudRate19200Datatype5OddParityStopbit1" , uvm_component parent = null);
+function UartSample13BaudRate19200Datatype5OddParityStopbit1:: new(string name = "UartSample13BaudRate19200Datatype5OddParityStopbit1" , uvm_component parent = null);
   super.new(name,parent);
 endfunction  : new
 //--------------------------------------------------------------------------------------------
