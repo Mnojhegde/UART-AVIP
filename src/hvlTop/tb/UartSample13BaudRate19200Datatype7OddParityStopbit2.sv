c76b40300125accb6da7bdae1cb0d22c66400c30
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2025-02-14 10:09:41 +0530

    Update UartSample13BaudRate19200Datatype7OddParityStopbit2.sv

--- src/hvlTop/tb/UartSample13BaudRate19200Datatype7OddParityStopbit2.sv
+++ src/hvlTop/tb/UartSample13BaudRate19200Datatype7OddParityStopbit2.sv
@@ -1,5 +1,5 @@
-`ifndef  UARTSAMPLE13BAUDRATE1920DATATYPE7ODDPARITYSTOPBIT2_INCLUDED_
-`define  UARTSAMPLE13BAUDRATE1920DATATYPE7ODDPARITYSTOPBIT2_INCLUDED_   
+`ifndef UARTSAMPLE13BAUDRATE1920DATATYPE7ODDPARITYSTOPBIT2_INCLUDED_
+`define UARTSAMPLE13BAUDRATE1920DATATYPE7ODDPARITYSTOPBIT2_INCLUDED_   
 //--------------------------------------------------------------------------------------------
 // Class: UartSample13BaudRate19200Datatype7OddParityStopbit2
 // A test for 13 sampling condition
@@ -10,11 +10,11 @@ class UartSample13BaudRate19200Datatype7OddParityStopbit2 extends UartBaseTest;
     //-------------------------------------------------------
     // Externally defined Tasks and Functions
     //-------------------------------------------------------
-    extern function new(string name = " UartSample13BaudRate19200Datatype7OddParityStopbit1" , uvm_component parent = null);
+    extern function new(string name = "UartSample13BaudRate19200Datatype7OddParityStopbit1" , uvm_component parent = null);
     extern virtual function void  build_phase(uvm_phase phase);
     extern virtual task run_phase(uvm_phase phase);
  
-endclass : UartSample13BaudRate19200Datatype7OddParityStopbit2
+endclass :UartSample13BaudRate19200Datatype7OddParityStopbit2
 //--------------------------------------------------------------------------------------------
 // Constructor:new
 //
@@ -22,7 +22,7 @@ endclass : UartSample13BaudRate19200Datatype7OddParityStopbit2
 //
 // parent - parent under which this component is created
 //--------------------------------------------------------------------------------------------
-function UartSample13BaudRate19200Datatype7OddParityStopbit2:: new(string name = " UartSample13BaudRate19200Datatype7OddParityStopbit1" , uvm_component parent = null);
+function UartSample13BaudRate19200Datatype7OddParityStopbit2:: new(string name = "UartSample13BaudRate19200Datatype7OddParityStopbit1" , uvm_component parent = null);
   super.new(name,parent);
 endfunction  : new
 //--------------------------------------------------------------------------------------------
