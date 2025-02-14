05bc51c023658282d7ac41f1eec51d3a40dbe7c6
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2025-02-14 10:22:04 +0530

    Update UartSample13BaudRate9600Datatype5EvenParityStopbit1.sv

--- src/hvlTop/tb/UartSample13BaudRate9600Datatype5EvenParityStopbit1.sv
+++ src/hvlTop/tb/UartSample13BaudRate9600Datatype5EvenParityStopbit1.sv
@@ -32,7 +32,7 @@ endfunction  : new
 // Parameters:
 //  phase - uvm phase
 //--------------------------------------------------------------------------------------------
-function voidUartSample13BaudRate9600Datatype5EvenParityStopbit1 :: build_phase(uvm_phase phase);
+function void UartSample13BaudRate9600Datatype5EvenParityStopbit1 :: build_phase(uvm_phase phase);
   super.build_phase(phase);
     uartEnvConfig.uartTxAgentConfig.uartOverSamplingMethod =OVERSAMPLING_13;
   uartEnvConfig.uartTxAgentConfig.uartBaudRate = BAUD_9600;
