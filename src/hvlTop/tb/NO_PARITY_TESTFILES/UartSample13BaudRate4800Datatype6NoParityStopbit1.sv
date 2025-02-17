efd7af9260b48e939bb0324383fe473c37c9754d
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2025-02-17 19:41:56 +0530

    Update UartSample13BaudRate4800Datatype6NoParityStopbit1.sv

--- src/hvlTop/tb/NO_PARITY_TESTFILES/UartSample13BaudRate4800Datatype6NoParityStopbit1.sv
+++ src/hvlTop/tb/NO_PARITY_TESTFILES/UartSample13BaudRate4800Datatype6NoParityStopbit1.sv
@@ -14,7 +14,7 @@ class UartSample13BaudRate4800Datatype6NoParityStopbit1 extends UartBaseTest;
     extern virtual function void  build_phase(uvm_phase phase);
     extern virtual task run_phase(uvm_phase phase);
 
-endclass :UartSample13BaudRate4800Datatype6EvenParityStopbit1
+endclass :UartSample13BaudRate4800Datatype6NoParityStopbit1
 //--------------------------------------------------------------------------------------------
 // Constructor:new
 //
