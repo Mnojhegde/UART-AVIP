91fa9b8200485ec4954e8be2091ed409ea4b38ff
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2025-02-17 20:10:45 +0530

    Update UartSample13BaudRate19200Datatype5NoParityStopbit2.sv

--- src/hvlTop/tb/NO_PARITY_TESTFILES/UartSample13BaudRate19200Datatype5NoParityStopbit2.sv
+++ src/hvlTop/tb/NO_PARITY_TESTFILES/UartSample13BaudRate19200Datatype5NoParityStopbit2.sv
@@ -4,17 +4,17 @@
 // Class:UartSample13BaudRate19200Datatype5NoParityStopbit2
 // A test for 13 sampling condition
 //--------------------------------------------------------------------------------------------
-class UartSample13BaudRate19200Datatype5NoParityStopbit2.sv extends UartBaseTest;
+class UartSample13BaudRate19200Datatype5NoParityStopbit2 extends UartBaseTest;
   `uvm_component_utils(UartSample13BaudRate19200Datatype5NoParityStopbit2.sv)
     UartVirtualBaseSequence uartVirtualBaseSequence;
     //-------------------------------------------------------
     // Externally defined Tasks and Functions
     //-------------------------------------------------------
-  extern function new(string name = "UartSample13BaudRate19200Datatype5NoParityStopbit2.sv" , uvm_component parent = null);
+  extern function new(string name = "UartSample13BaudRate19200Datatype5NoParityStopbit2" , uvm_component parent = null);
     extern virtual function void  build_phase(uvm_phase phase);
     extern virtual task run_phase(uvm_phase phase);
 
-endclass :UartSample13BaudRate19200Datatype5NoParityStopbit2.sv
+endclass :UartSample13BaudRate19200Datatype5NoParityStopbit2
 //--------------------------------------------------------------------------------------------
 // Constructor:new
 //
@@ -22,7 +22,7 @@ endclass :UartSample13BaudRate19200Datatype5NoParityStopbit2.sv
 //
 // parent - parent under which this component is created
 //--------------------------------------------------------------------------------------------
-function UartSample13BaudRate19200Datatype5NoParityStopbit2.sv:: new(string name = "UartSample13BaudRate19200Datatype5NoParityStopbit2.sv" , uvm_component parent = null);
+function UartSample13BaudRate19200Datatype5NoParityStopbit2:: new(string name = "UartSample13BaudRate19200Datatype5NoParityStopbit2" , uvm_component parent = null);
   super.new(name,parent);
 endfunction  : new
 //--------------------------------------------------------------------------------------------
@@ -32,7 +32,7 @@ endfunction  : new
 // Parameters:
 //  phase - uvm phase
 //--------------------------------------------------------------------------------------------
-function void UartSample13BaudRate19200Datatype5NoParityStopbit2.sv:: build_phase(uvm_phase phase);
+function void UartSample13BaudRate19200Datatype5NoParityStopbit2:: build_phase(uvm_phase phase);
   super.build_phase(phase);
   uartEnvConfig.uartTxAgentConfig.uartOverSamplingMethod =OVERSAMPLING_13;
   uartEnvConfig.uartTxAgentConfig.uartBaudRate = BAUD_19200;
@@ -55,7 +55,7 @@ endfunction  : build_phase
 // Parameters:
 // phase - stores the current phase
 //------------------------------------------------------------------------------------------
-task UartSample13BaudRate19200Datatype5NoParityStopbit2.sv:: run_phase(uvm_phase phase);
+task UartSample13BaudRate19200Datatype5NoParityStopbit2:: run_phase(uvm_phase phase);
   super.run_phase(phase);
   endtask : run_phase
 `endif
