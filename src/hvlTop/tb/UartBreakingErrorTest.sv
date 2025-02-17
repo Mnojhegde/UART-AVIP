582d71a78120ca85a4c2099cf88cd627b213f380
Seema J Rai <109678174+seemaRai902@users.noreply.github.com>
2025-02-17 12:12:28 +0530

    Create UartBreakingErrorTest.sv

---
+++ src/hvlTop/tb/UartBreakingErrorTest.sv
@@ -0,0 +1,77 @@
+`ifndef UARTBREAKINGERRORTEST_INCLUDED_
+`define UARTBREAKINGERRORTEST_INCLUDED_ 
+
+//--------------------------------------------------------------------------------------------
+// Class:  UartBreakingErrorTest
+// A test for Breaking error check
+//--------------------------------------------------------------------------------------------
+class UartBreakingErrorTest extends UartBaseTest;
+ 
+   `uvm_component_utils(UartBreakingErrorTest)
+    
+   UartVirtualBaseSequence uartVirtualBaseSequence;
+    //-------------------------------------------------------
+    // Externally defined Tasks and Functions
+    //-------------------------------------------------------
+    extern function new(string name = "UartBreakingErrorTest" , uvm_component parent = null);
+    extern virtual function void  build_phase(uvm_phase phase);
+    extern virtual task run_phase(uvm_phase phase);
+
+endclass : UartBreakingErrorTest
+		     
+//--------------------------------------------------------------------------------------------
+// Constructor:new
+//
+// Paramters:
+//
+// parent - parent under which this component is created
+//--------------------------------------------------------------------------------------------
+function UartBreakingErrorTest :: new(string name = "UartBreakingErrorTest" , uvm_component parent = null);
+  super.new(name,parent);
+endfunction  : new
+		          
+//--------------------------------------------------------------------------------------------
+// Function: build_phase
+//  Create required ports
+//
+// Parameters:
+//  phase - uvm phase
+//--------------------------------------------------------------------------------------------
+function void UartBreakingErrorTest :: build_phase(uvm_phase phase);
+  super.build_phase(phase);
+  //uartEnvConfig.uartTxAgentConfig.uartOverSamplingMethod = OVERSAMPLING_16
+// uartEnvConfig.uartTxAgentConfig.uartBaudRate =   BAUD_9600;
+  uartEnvConfig.uartTxAgentConfig.uartDataType = SEVEN_BIT;
+  uartEnvConfig.uartTxAgentConfig.uartParityType = EVEN_PARITY;
+  uartEnvConfig.uartTxAgentConfig.uartStopBit = TWO_BIT;
+  uartEnvConfig.uartTxAgentConfig.hasParity=1;
+  uartEnvConfig.uartTxAgentConfig.patternNeeded = 'b0; 
+
+
+ // uartEnvConfig.uartRxAgentConfig.uartOverSamplingMethod = OVERSAMPLING_16;
+  //uartEnvConfig.uartRxAgentConfig.uartBaudRate =   BAUD_9600;
+  uartEnvConfig.uartRxAgentConfig.uartDataType = SEVEN_BIT;
+  uartEnvConfig.uartRxAgentConfig.uartParityType = EVEN_PARITY;
+  uartEnvConfig.uartRxAgentConfig.uartStopBit = TWO_BIT;
+  uartEnvConfig.uartRxAgentConfig.hasParity=1;
+
+endfunction  : build_phase
+			         
+				    
+//--------------------------------------------------------------------------------------------
+// task:body
+// Creates the required ports
+//
+// Parameters:
+// phase - stores the current phase
+//------------------------------------------------------------------------------------------
+task UartBreakingErrorTest:: run_phase(uvm_phase phase);
+  UartVirtualBaseSequence :: type_id ::set_type_override(UartVirtualTransmissionSequence::get_type());
+  uartVirtualBaseSequence = UartVirtualBaseSequence :: type_id :: create("uartVirtualBaseSequence");
+  uartVirtualBaseSequence.print();
+  phase.raise_objection(this);
+  uartVirtualBaseSequence.start(uartEnv.uartVirtualSequencer);
+  #100000;
+  phase.drop_objection(this);
+  endtask : run_phase
+`endif  
