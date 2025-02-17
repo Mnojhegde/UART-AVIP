af8ca9f1618a0518f4845c9b563a140872fdeb3b
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2025-02-17 19:09:21 +0530

    Update UartSample13BaudRate4800Datatype5NoParityStopbit1.sv

--- src/hvlTop/tb/NO_PARITY_TESTFILES/UartSample13BaudRate4800Datatype5NoParityStopbit1.sv
+++ src/hvlTop/tb/NO_PARITY_TESTFILES/UartSample13BaudRate4800Datatype5NoParityStopbit1.sv
@@ -1,26 +1,20 @@
-
-`ifndef UartSample13BaudRate4800Datatype5NoParityStopbit1
-`define UartSample13BaudRate4800Datatype5NoParityStopbit1
-
+`ifndef UARTSAMPLE13BAUDRATE4800DATATYPE5NOPARITYSTOPBIT1_INCLUDED_
+`define UARTSAMPLE13BAUDRATE4800DATATYPE5NOPARITYSTOPBIT1_INCLUDED_
 //--------------------------------------------------------------------------------------------
-// Class: UartSample16BaudRate19200Datatype5NoParityStopbit1
-// Base test has the test scenarios for testbench which has the env, config, etc.
-// Sequences are created and started in the test
+// Class:UartSample13BaudRate4800Datatype5NoParityStopbit1
+// A test for 13 sampling condition
 //--------------------------------------------------------------------------------------------
-class UartSample16BaudRate19200Datatype5NoParityStopbit1 extends UartBaseTest;
- 
-  `uvm_component_utils(UartSample16BaudRate19200Datatype5NoParityStopbit1)
- 
-  UartVirtualBaseSequence uartVirtualBaseSequence;
-  //-------------------------------------------------------
-  // Externally defined Tasks and Functions
-  //-------------------------------------------------------
-  extern function new(string name = "UartSample16BaudRate19200Datatype5NoParityStopbit1" , uvm_component parent = null);
-  extern virtual function void  build_phase(uvm_phase phase);
-  extern virtual task run_phase(uvm_phase phase);
+class UartSample13BaudRate4800Datatype5NoParityStopbit1 extends UartBaseTest;
+ `uvm_component_utils( UartSample13BaudRate4800Datatype5NoParityStopbit1)
+    UartVirtualBaseSequence uartVirtualBaseSequence;
+    //-------------------------------------------------------
+    // Externally defined Tasks and Functions
+    //-------------------------------------------------------
+ extern function new(string name = "UartSample13BaudRate4800Datatype5NoParityStopbit1" , uvm_component parent = null);
+    extern virtual function void  build_phase(uvm_phase phase);
+    extern virtual task run_phase(uvm_phase phase);
 
-endclass : UartSample16BaudRate19200Datatype5NoParityStopbit1
-   
+endclass :UartSample13BaudRate4800Datatype5EvenParityStopbit1
 //--------------------------------------------------------------------------------------------
 // Constructor:new
 //
@@ -28,10 +22,9 @@ endclass : UartSample16BaudRate19200Datatype5NoParityStopbit1
 //
 // parent - parent under which this component is created
 //--------------------------------------------------------------------------------------------
-function UartSample16BaudRate19200Datatype5NoParityStopbit1 :: new(string name = "UartSample16BaudRate19200Datatype5NoParityStopbit1" , uvm_component parent = null);
+     function UartSample13BaudRate4800Datatype5NoParityStopbit1:: new(string name = "UartSample13BaudRate4800Datatype5NoParityStopbit1" , uvm_component parent = null);
   super.new(name,parent);
 endfunction  : new
-   
 //--------------------------------------------------------------------------------------------
 // Function: build_phase
 //  Create required ports
@@ -39,31 +32,32 @@ endfunction  : new
 // Parameters:
 //  phase - uvm phase
 //--------------------------------------------------------------------------------------------
-function void UartSample16BaudRate19200Datatype5NoParityStopbit1 :: build_phase(uvm_phase phase);
+function void UartSample13BaudRate4800Datatype5NoParityStopbit1 :: build_phase(uvm_phase phase);
   super.build_phase(phase);
-  uartEnvConfig.uartTxAgentConfig.uartOverSamplingMethod = OVERSAMPLING_16;
-  uartEnvConfig.uartTxAgentConfig.uartBaudRate =   BAUD_9600;
+  uartEnvConfig.uartTxAgentConfig.uartOverSamplingMethod =OVERSAMPLING_13;
+  uartEnvConfig.uartTxAgentConfig.uartBaudRate = BAUD_4800;
   uartEnvConfig.uartTxAgentConfig.uartDataType = FIVE_BIT;
-  uartEnvConfig.uartTxAgentConfig.uartStopBit = ONE_BIT;
-  uartEnvConfig.uartTxAgentConfig.hasParity = PARITY_DISABLED;
+  uartEnvConfig.uartTxAgentConfig.uartParityType = EVEN_PARITY;
+  uartEnvConfig.uartTxAgentConfig.uartStopBit = PARITY_DISABLED;
+  uartEnvConfig.uartTxAgentConfig.hasParity=1;
 
-  uartEnvConfig.uartRxAgentConfig.uartOverSamplingMethod = OVERSAMPLING_16;
-  uartEnvConfig.uartRxAgentConfig.uartBaudRate =   BAUD_9600;
+     uartEnvConfig.uartRxAgentConfig.uartOverSamplingMethod =OVERSAMPLING_13;
+  uartEnvConfig.uartRxAgentConfig.uartBaudRate = BAUD_4800;
   uartEnvConfig.uartRxAgentConfig.uartDataType = FIVE_BIT;
+  uartEnvConfig.uartRxAgentConfig.uartParityType = EVEN_PARITY;
   uartEnvConfig.uartRxAgentConfig.uartStopBit = ONE_BIT;
-  uartEnvConfig.uartRxAgentConfig.hasParity = PARITY_DISABLED;
+  uartEnvConfig.uartRxAgentConfig.hasParity=PARITY_DISABLED;
+
 endfunction  : build_phase
-   
-   
+
 //--------------------------------------------------------------------------------------------
 // task:body
 // Creates the required ports
 //
 // Parameters:
 // phase - stores the current phase
-//--------------------------------------------------------------------------------------------
- task UartSample16BaudRate19200Datatype5NoParityStopbit1 :: run_phase(uvm_phase phase);
-    super.run_phase(phase);
-endtask : run_phase
-
-`endif  
+//------------------------------------------------------------------------------------------
+task UartSample13BaudRate4800Datatype5NoParityStopbit1:: run_phase(uvm_phase phase);
+  super.run_phase(phase);
+  endtask : run_phase
+`endif
