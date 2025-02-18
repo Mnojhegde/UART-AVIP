8a383d916e4bae797d0791430c815e0446db2a3e
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2025-02-18 11:49:42 +0530

    Update UartSample13BaudRate9600Datatype6NoParityStopbit1.sv

--- src/hvlTop/tb/UartSample13BaudRate9600Datatype6NoParityStopbit1.sv
+++ src/hvlTop/tb/UartSample13BaudRate9600Datatype6NoParityStopbit1.sv
@@ -10,7 +10,7 @@ class UartSample13BaudRate9600Datatype6NoParityStopbit1 extends UartBaseTest;
     //-------------------------------------------------------
     // Externally defined Tasks and Functions
     //-------------------------------------------------------
-  extern function new(string name = "UartSample13BaudRate9600Datatype56NoParityStopbit1" , uvm_component parent = null);
+  extern function new(string name = "UartSample13BaudRate9600Datatype6NoParityStopbit1" , uvm_component parent = null);
     extern virtual function void  build_phase(uvm_phase phase);
     extern virtual task run_phase(uvm_phase phase);
 
