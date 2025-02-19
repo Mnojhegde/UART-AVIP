f120001051a40bff3d26a4cc1ff6c36f4c337193
Mnojhegde <109811963+Mnojhegde@users.noreply.github.com>
2025-02-19 11:06:50 +0530

    Update UartTxMonitorBfm.sv

--- src/hdlTop/uartTxAgentBfm/UartTxMonitorBfm.sv
+++ src/hdlTop/uartTxAgentBfm/UartTxMonitorBfm.sv
@@ -163,7 +163,6 @@ interface UartTxMonitorBfm (input  logic   clk,
 						breakZeroCount=uartConfigStruct.uartParityEnable ? (uartConfigStruct.uartDataType)+4 :(uartConfigStruct.uartDataType)+3;
 					else
 						breakZeroCount=uartConfigStruct.uartParityEnable ? (uartConfigStruct.uartDataType)+3 :(uartConfigStruct.uartDataType)+2;
-					$display("******* N.O.Z=%0d ******** B.Z.C.=%0d **************",numOfZeroes,breakZeroCount);
 					if(numOfZeroes == breakZeroCount)
 						uartTxPacketStruct.breakingError =1;
 				  else 
