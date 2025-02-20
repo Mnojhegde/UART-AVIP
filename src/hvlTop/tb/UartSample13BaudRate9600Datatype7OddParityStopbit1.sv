7045ddbd668ad76ce64b5f9ca5608a693d6eb438
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2025-02-20 12:17:16 +0530

    Update UartSample13BaudRate9600Datatype7OddParityStopbit1.sv

--- src/hvlTop/tb/UartSample13BaudRate9600Datatype7OddParityStopbit1.sv
+++ src/hvlTop/tb/UartSample13BaudRate9600Datatype7OddParityStopbit1.sv
@@ -33,7 +33,9 @@ endfunction  : new
 //  phase - uvm phase
 //--------------------------------------------------------------------------------------------
 function void UartSample13BaudRate9600Datatype7OddParityStopbit1 :: build_phase(uvm_phase phase);
-  super.build_phase(phase);uartEnvConfig.uartTxAgentConfig.uartBaudRate = BAUD_9600;
+  super.build_phase(phase);
+   uartEnvConfig.uartTxAgentConfig.uartOverSamplingMethod =OVERSAMPLING_13;
+   uartEnvConfig.uartTxAgentConfig.uartBaudRate = BAUD_9600;
   uartEnvConfig.uartTxAgentConfig.uartDataType = SEVEN_BIT;
   uartEnvConfig.uartTxAgentConfig.uartParityType = ODD_PARITY;
   uartEnvConfig.uartTxAgentConfig.uartStopBit = ONE_BIT;
