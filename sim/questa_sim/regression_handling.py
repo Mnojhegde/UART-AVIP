debc9ef09272167ce8d6c691ce5055f3904bf052
Seema J Rai <109678174+seemaRai902@users.noreply.github.com>
2025-02-20 13:02:35 +0530

    Update regression_handling.py

--- sim/questa_sim/regression_handling.py
+++ sim/questa_sim/regression_handling.py
@@ -15,33 +15,42 @@ debug = 0
 # Get regression list as input
 regression_list = str(sys.argv[1])
 if debug:
-  print(regression_list);
+    print(regression_list)
 
 # Reading the file
 path = "../../src/hvl_top/tb/" + regression_list
 if debug:
-  print(path)
+    print(path)
 
-with open(path,'r') as f:
-  for line in f:
-    match = re.search('\#',line);
-    if(match): 
-      continue;
+with open(path, 'r') as f:
+    for line in f:
+        match = re.search('#', line)
+        if match:
+            continue
 
-    match = re.search('(*StopBit%d)',line);
-    if(match):
-      reg_list_array.append(match.group(0))
-      if debug:
-        print(match.group(0));
+        match = re.search('(*StopBit\d+)', line)
+        if match:
+            reg_list_array.append(match.group(0))
+            if debug:
+                print(match.group(0))
 
-for testname in reg_list_array:
-  timestr = time.strftime( "%d%m%Y-%H%M%S" )
-  if debug:
-    print( "time string : ", timestr )
+# Prepare the trace file to record the test names
+trace_file_path = "test_names_trace.txt"
+with open(trace_file_path, 'a') as trace_file:
+    for testname in reg_list_array:
+        timestr = time.strftime("%d%m%Y-%H%M%S")
+        if debug:
+            print("time string : ", timestr)
 
-  test_folder = testname + "_" + timestr
+        test_folder = testname + "_" + timestr
 
-  line = "make simulate test_folder=" + test_folder + " test=" + testname
-  if debug:
-    print(line)
-  os.system(line)
+        line = "make simulate test_folder=" + test_folder + " test=" + testname
+        if debug:
+            print(line)
+        
+        # Write the test name to the trace file
+        trace_file.write(testname + "\n")
+
+        os.system(line)
+
+print(f"Test names have been traced in {trace_file_path}")
