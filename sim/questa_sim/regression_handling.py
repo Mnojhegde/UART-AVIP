f48f913da906e90455324111c87cced015507e48
Mnojhegde <109811963+Mnojhegde@users.noreply.github.com>
2025-02-20 14:56:53 +0530

    Update regression_handling.py

--- sim/questa_sim/regression_handling.py
+++ sim/questa_sim/regression_handling.py
@@ -18,7 +18,7 @@ if debug:
     print(regression_list)
 
 # Reading the file
-path = "../../src/hvl_top/tb/regressionTest" + regression_list
+path = "../../src/hvlTop/tb/regressionTest" + regression_list
 if debug:
     print(path)
 
