bc7e402170325e78fb263615033ef0fcd0964a15
Mnojhegde <109811963+Mnojhegde@users.noreply.github.com>
2025-02-26 12:59:00 +0530

    Update UartOddParityWithErrorTest.sv

--- src/hvlTop/tb/UartOddParityWithErrorTest.sv
+++ src/hvlTop/tb/UartOddParityWithErrorTest.sv
@@ -43,6 +43,8 @@ function void UartOddParityWithErrorTest :: build_phase(uvm_phase phase);
   uartEnvConfig.uartTxAgentConfig.hasParity = PARITY_ENABLED;
   uartEnvConfig.uartTxAgentConfig.uartParityType = ODD_PARITY;
   uartEnvConfig.uartTxAgentConfig.parityErrorInjection =1;
+	uartEnvConfig.uartRxAgentConfig.parityErrorInjection =1;
+
 endfunction  : build_phase
 			         
 				    
